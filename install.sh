e4e3220092c31fd71c93b719883219ac3096fd52
joserf <joserodriguesfilho@gmail.com>
2019-12-27 00:31:34 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+clear
+
+if [ "$UID" -ne 0 ]; then
+  echo "Por favor, execute como root"
+  exit 1
+fi
+
+read -p 'Digite o ip do servidor autorizado para coletar os dados SNMP :' SNMP_IP
+read -p 'COMMUNITY ex: public: ' COMMUNITY
+read -p 'Digite o seu e-mail : ' EMAIL
+read -p 'Digite o local ou identificacao desse servidor :' LOCAL
+
+clear
+
+echo IP autorizado: $SNMP_IP
+echo COMMUNITY: $COMMUNITY
+echo E-mail: $EMAIL 
+echo Local: $LOCAL && sleep 10
+
+
+if [ -x /usr/bin/apt-get ]; then
+  # Instala os pacotes 
+  apt-get update && \
+  apt-get install snmpd -y && \
+  # parando SNMP 
+  /etc/init.d/snmpd stop \
+  # BKP configuracoes
+  cp /etc/snmp/snmpd.conf /etc/snmp/snmpd.bkp
+  # efetua as configuracoes
+  sed -i "s/agentAddress  udp:127.0.0.1:161/agentAddress  udp:0.0.0.0:161/" /etc/snmp/snmpd.conf
+  sed -i '47i\view   all         included   .1                80' /etc/snmp/snmpd.conf 
+  sed -i "s/rocommunity public  default    -V systemonly/#rocommunity public  default    -V systemonly/" /etc/snmp/snmpd.conf
+  sed -i "58i\rocommunity $COMMUNITY  $SNMP_IP" /etc/snmp/snmpd.conf
+  sed -i "s/sysContact     Me <me@example.org>/sysContact     <$EMAIL>/" /etc/snmp/snmpd.conf
+  sed -i "s/sysLocation    Sitting on the Dock of the Bay/sysLocation    $LOCAL/" /etc/snmp/snmpd.conf
+  #inicia o SNMP
+  /etc/init.d/snmpd start
+
+  option="${1}" 
+  case ${option} in 
+     -h|-H) 
+        # Exibe a versão 
+        echo "José Rodrigues Filho"
+        echo "SNMP | V1.0"
+        ;;
+     *)  
+        echo "`basename ${0}`:Opção inválida, use: [-h Versão]" 
+        exit 1
+        ;; 
+  esac
+EOF
+
+#
+
+  exit 0
+fi
